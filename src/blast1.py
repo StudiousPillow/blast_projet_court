f9c0164d125a5c4be1256b5b37fa55953a6ef192
StudiousPillow <nicolas.aubourg@etu.u-paris.fr>
2024-09-06 10:49:05 +0200

    kuplets1

--- src/blast1.py
+++ src/blast1.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd ## alternative ?
 
 class Sequence:
+    bases = ['A','C','T','G']
     dna_score_matrix = pd.DataFrame(np.array([[5,-4,-4,-4],[-4,5,-4,-4],[-4,-4,5,-4],[-4,-4,-4,5]]), index=["A","C","T","G"], columns = ["A","C","T","G"])
     def __init__(self, seq):
         self.seq = seq
@@ -9,7 +10,25 @@ class Sequence:
         return(self.seq[idx:idx+k])
     def generate_kuplet(self, score_lim):
         pass
-    # def calc_score(self, ):
+    def calc_score(self, seq):
+        score = 0
+        for i in range(len(self.seq)):
+            score += self.dna_score_matrix.loc[self.seq[i], seq.seq[i]]
+        return(score)
+    def get_muted(self, idx, replacement):
+        lseq = self.seq.split('') 
+        lseq[idx] = replacement
+        seq = ''.join(lseq)
+        return(Sequence(seq))
+        
+    def generate_kuplet(self, T):
+        kuplets = []
 
-S = Sequence("ACT")
-print(S.dna_score_matrix)
+            
+
+    
+
+S1 = Sequence("ACTTTGCCGC")
+S2 = Sequence("AGGGGGCCGC")
+print(S1.calc_score(S2))
+
